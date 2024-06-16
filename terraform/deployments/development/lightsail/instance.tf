e77e40bf8d1bd085e9b19fb0934bdff260de2fd5
Sergio Flores <byoigres@gmail.com>
2024-06-16 13:44:33 -0700

    Add 443 and 6001 ports.

--- terraform/deployments/development/lightsail/instance.tf
+++ terraform/deployments/development/lightsail/instance.tf
@@ -27,8 +27,20 @@ resource "aws_lightsail_instance_public_ports" "instance_ports" {
 
   port_info {
     protocol  = "tcp"
+    from_port = 443
+    to_port   = 443
+  }
+
+  port_info {
+    protocol  = "tcp"
     from_port = 8000
     to_port   = 8000
+  }
+
+  port_info {
+    protocol  = "tcp"
+    from_port = 6001
+    to_port   = 6001
   }
 }
 
